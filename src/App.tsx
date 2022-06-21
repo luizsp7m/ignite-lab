1909c330ccfda35985a614d6abfc745d05766ada
Luiz Oliveira <luizoliveira2808@gmail.com>
2022-06-20 22:19:07 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons: {
+      id
+      title
+    }
+  }
+`;
+
+export function App() {
+  const { data } = useQuery(GET_LESSONS_QUERY);
+
+  return <h1>Hello React</h1>
+}
